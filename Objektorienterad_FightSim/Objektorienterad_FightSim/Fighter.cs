231d3227c0dad1727996ef76974fd31b7e964f67
GUSTOfWinds <gustavkjonsson01@gmail.com>
2019-10-09 15:01:47 +0200

    Summary 09/10

---
+++ Objektorienterad_FightSim/Objektorienterad_FightSim/Fighter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objektorienterad_FightSim
+{
+    class Fighter
+    {
+        int hp;
+        string name;
+        Random generator = new Random();
+
+        public Fighter()
+        {
+            hp = 45;
+            hp += generator.Next(1, 11);
+            Console.WriteLine("Enter the name of the fighter");
+            name = Console.ReadLine();
+            Console.WriteLine("The Fighter " + name + " enters the fray with " + hp + " health!");
+        }
+
+        public int Attack()
+        {
+            int damagedealt = generator.Next(1, 11);
+
+            return damagedealt;
+        }
+        public void Hurt(int amount)
+        {
+            hp -= amount;
+        }
+        public int getHP()
+        {
+            return hp;
+        }
+        public bool IsAlive()
+        {
+            if (hp >= 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- Objektorienterad_FightSim/Objektorienterad_FightSim/Program.cs
+++ Objektorienterad_FightSim/Objektorienterad_FightSim/Program.cs
@@ -10,6 +10,24 @@ namespace Objektorienterad_FightSim
     {
         static void Main(string[] args)
         {
+            Fighter A = new Fighter();
+            Fighter B = new Fighter();
+            Console.WriteLine("The Fight Begins!");
+            Console.ReadLine();
+
+            while (A.IsAlive() && B.IsAlive())
+            {
+
+            }
+
+            if (A.IsAlive() == true)
+            {
+                //B WON!
+            }
+            else
+            {
+                //A WON!
+            }
 
         }
     }
